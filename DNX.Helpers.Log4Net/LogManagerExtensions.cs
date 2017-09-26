f2c9bb3a6390ac09ab2bebe4d823de2102c093eb
Martin Smith <martinsmith1968@gmail.com>
2017-09-26 12:51:49 +0100

    Added Log4Net extensions (needs separate NuGet feed)

---
+++ DNX.Helpers.Log4Net/LogManagerExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using log4net;
+
+namespace DNX.Helpers.Log4Net
+{
+    public static class LogManagerExtensions
+    {
+        public static ILog GetDefaultLogger()
+        {
+            return LogManager.GetLogger(string.Empty);
+        }
+
+        public static ILog GetLoggerFor(object parent)
+        {
+            return parent == null
+                ? GetDefaultLogger()
+                : LogManager.GetLogger(parent.GetType());
+        }
+
+        public static ILog GetMyLogger()
+        {
+            var trace = new StackTrace();
+            var frame = trace.GetFrame(1);
+            var caller = frame == null ? null : frame.GetMethod();
+
+            return caller == null
+                ? GetDefaultLogger()
+                : LogManager.GetLogger(caller.DeclaringType);
+        }
+
+        public static ILog GetLogger<T>()
+        {
+            return LogManager.GetLogger(typeof(T));
+        }
+    }
+}
---
+++ Test.DNX.Helpers.Log4Net/LogExtensionsTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using DNX.Helpers.Log4Net;
+using log4net;
+using Moq;
+using NUnit.Framework;
+
+namespace Test.DNX.Helpers.Log4Net
+{
+    [TestFixture]
+    public class LogExtensionsTests
+    {
+        [Test]
+        public void DebugIfEnabled_DebugNotEnabled_TextNotEvaluated()
+        {
+            // Arrange
+            var called = false;
+
+            var logger = new Mock<ILog>(MockBehavior.Strict);
+            logger.Setup(l => l.IsDebugEnabled).Returns(false);
+
+            Func<string> getText = () =>
+            {
+                called = true;
+
+                return "Some text";
+            };
+
+            // Act
+            logger.Object.DebugFunc(getText);
+
+            // Assert
+            Assert.IsFalse(called);
+            logger.Verify(l => l.Debug(It.IsAny<object>()), Times.Never());
+        }
+
+        [Test]
+        public void DebugIfEnabled_DebugEnabled_TextEvaluated()
+        {
+            // Arrange
+            var called = false;
+
+            var logger = new Mock<ILog>(MockBehavior.Strict);
+            logger.Setup(l => l.IsDebugEnabled).Returns(true);
+            logger.Setup(l => l.Debug(It.IsAny<object>()));
+
+            Func<string> getText = () =>
+            {
+                called = true;
+
+                return "Some text";
+            };
+
+            // Act
+            logger.Object.DebugFunc(getText);
+
+            // Assert
+            Assert.IsTrue(called);
+            logger.Verify(l => l.Debug(It.IsAny<object>()), Times.Once());
+        }
+
+        [Test]
+        public void DebugIfEnabled_DebugEnabled_TextEvaluated2()
+        {
+            // Arrange
+            var called = false;
+
+            var logger = new Mock<ILog>();
+            logger.Setup(l => l.IsDebugEnabled).Returns(true);
+
+            Func<int> getNumber = () =>
+            {
+                called = true;
+
+                return DateTime.UtcNow.Millisecond;
+            };
+
+            // Act
+            logger.Object.DebugFunc(() => string.Format("Value: {0}", getNumber()));
+
+            // Assert
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void DebugIfEnabled_DebugNotEnabled_TextNotEvaluated2()
+        {
+            // Arrange
+            var called = false;
+
+            var logger = new Mock<ILog>();
+            logger.Setup(l => l.IsDebugEnabled).Returns(false);
+
+            Func<int> getNumber = () =>
+            {
+                called = true;
+
+                return DateTime.UtcNow.Millisecond;
+            };
+
+            // Act
+            logger.Object.DebugFunc(() => string.Format("Value: {0}", getNumber));
+
+            // Assert
+            Assert.IsFalse(called);
+        }
+    }
+}

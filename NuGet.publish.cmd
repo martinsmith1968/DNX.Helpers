6b450bf90718c50f16c5016ef163bed32009f4ef
Martin Smith <martinsmith1968@gmail.com>
2017-10-04 14:02:30 +0100

    Package version updates and removal of Log4Net project(s)

--- NuGet.publish.cmd
+++ NuGet.publish.cmd
@@ -4,18 +4,19 @@ SETLOCAL EnableDelayedExpansion
 
 SET SCRIPTPATH=%~dp0
 
+SET APPTITLE=DNX.Helpers
+
 REM NuGet feed v3 (VS 2015 / NuGet v3.x): https://api.nuget.org/v3/index.json
 REM NuGet feed v2 (VS 2013 and earlier / NuGet 2.x): https://www.nuget.org/api/v2
 
 SET NUGETEXE=%SCRIPTPATH%\.nuget\nuget.exe
-
 SET NUGETSERVERAPIURL=https://www.nuget.org/api/v2/package
-rem SET NUGETSERVERAPIURL=https://www.nuget.org
-SET NUGETSERVERAPIKEY=1d9c8df7-5dbc-40d0-baef-b36a8c9244e2
+
+CALL SetNuGetServerAPIKey.cmd "%APPTITLE%"
 
 IF EXIST *.nupkg DEL *.nupkg
 
-"%NUGETEXE%" pack "%SCRIPTPATH%\DNX.Helpers\DNX.Helpers.csproj"
+"%NUGETEXE%" pack "%SCRIPTPATH%\%APPTITLE%\%APPTITLE%.csproj"
 
 FOR %%F IN (*.nupkg) DO (
     ECHO.Pushing: %%~F

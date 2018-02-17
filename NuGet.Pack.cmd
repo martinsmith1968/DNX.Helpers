f933ffe2c513115d97fa43f92ca1096597e9d364
Martin Smith <martinsmith1968@gmail.com>
2018-02-17 22:17:55 +0000

    Travis CI to nuget pack on build and nuget push on master build

--- NuGet.Pack.cmd
+++ NuGet.Pack.cmd
@@ -4,7 +4,7 @@ SETLOCAL EnableDelayedExpansion
 
 SET SCRIPTPATH=%~dp0
 
-SET APPTITLE=DNX.Helpers
+SET APPNAME=DNX.Helpers
 
 REM NuGet feed v3 (VS 2015 / NuGet v3.x): https://api.nuget.org/v3/index.json
 REM NuGet feed v2 (VS 2013 and earlier / NuGet 2.x): https://www.nuget.org/api/v2
@@ -12,8 +12,8 @@ REM NuGet feed v2 (VS 2013 and earlier / NuGet 2.x): https://www.nuget.org/api/v
 SET NUGETEXE=%SCRIPTPATH%\.nuget\nuget.exe
 SET NUGETSERVERAPIURL=https://www.nuget.org/api/v2/package
 
-CALL SetNuGetServerAPIKey.cmd "%APPTITLE%"
+CALL SetNuGetServerAPIKey.cmd "%APPNAME%"
 
 IF EXIST *.nupkg DEL *.nupkg
 
-"%NUGETEXE%" pack "%SCRIPTPATH%\%APPTITLE%\%APPTITLE%.csproj"
+"%NUGETEXE%" pack "%SCRIPTPATH%\%APPNAME%\%APPNAME%.csproj"
--- NuGet.Push.cmd
+++ NuGet.Push.cmd
@@ -6,7 +6,7 @@ SET SCRIPTPATH=%~dp0
 
 CALL "%SCRIPTPATH%\NuGet.Package.cmd"
 
-SET APPTITLE=DNX.Helpers
+SET APPNAME=DNX.Helpers
 
 REM NuGet feed v3 (VS 2015 / NuGet v3.x): https://api.nuget.org/v3/index.json
 REM NuGet feed v2 (VS 2013 and earlier / NuGet 2.x): https://www.nuget.org/api/v2
@@ -14,7 +14,7 @@ REM NuGet feed v2 (VS 2013 and earlier / NuGet 2.x): https://www.nuget.org/api/v
 SET NUGETEXE=%SCRIPTPATH%\.nuget\nuget.exe
 SET NUGETSERVERAPIURL=https://www.nuget.org/api/v2/package
 
-CALL SetNuGetServerAPIKey.cmd "%APPTITLE%"
+CALL SetNuGetServerAPIKey.cmd "%APPNAME%"
 
 FOR %%F IN (*.nupkg) DO (
     ECHO.Pushing: %%~F

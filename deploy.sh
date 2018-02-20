4f9c0d0fcc4c0de5cc938a80f87dc993166d58ca
Martin Smith <martin.smith@clear.bank>
2018-02-20 15:57:49 +0000

    Move auto deploy to deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+APPNAME=$1
+NUGET_API_URL=$2
+NUGET_API_KEY=$3
+
+echo nuget push $APPNAME.*.nupkg -Source $NUGET_API_URL -ApiKey secret
+nuget push $APPNAME.*.nupkg -Source $NUGET_API_URL -ApiKey $NUGET_API_KEY
+  

41f3d53e4d022d9eca97ebf8e5287db28807d76d
Martin Smith <martinsmith1968@gmail.com>
2019-12-28 13:38:37 +0000

    Remove deploy.sh file no longer needed

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-APPNAME=$1
-NUGET_API_URL=$2
-NUGET_API_KEY=$3
-
-echo nuget push $APPNAME.*.nupkg -Source $NUGET_API_URL -ApiKey secret
-nuget push $APPNAME.*.nupkg -Source $NUGET_API_URL -ApiKey $NUGET_API_KEY
